066f4a055eb1f5ffe9bd2f3e50aeacd48232567a
Oleksandr Chernov <alexsxchernov@gmail.com>
2024-10-24 17:24:56 +0100

    (59)

--- DemoApp/Support/staging/Target.swift
+++ DemoApp/Support/staging/Target.swift
@@ -12,12 +12,14 @@ import UIKit
 
 enum Target {
     static let customers = [
+        Customer(id: "TkPnD3_yP3j6dUEAksgRjJ-auYijyUECLxVEnFqHJVE=", name: "STG-Test2"),
         Customer(id: "CzWgbh_Y0-Lod0VCjhwkiIDt5y3QxLLcoy0FcEDoc9E=", name: "STG-Test"),
     ]
     
     static func branding(for c: Customer) -> Branding {
         switch c.id {
-        case "CzWgbh_Y0-Lod0VCjhwkiIDt5y3QxLLcoy0FcEDoc9E=": // Staging
+        case "TkPnD3_yP3j6dUEAksgRjJ-auYijyUECLxVEnFqHJVE=",
+            "CzWgbh_Y0-Lod0VCjhwkiIDt5y3QxLLcoy0FcEDoc9E=": // Staging
             return Branding(primary: UIColor(red: 0, green: 0.25, blue: 0.125, alpha: 1),
                             secondary: UIColor(red: 0.8, green: 0.05, blue: 0.2, alpha: 1),
                             fontName: "AmericanTypewriter",
